cecf579c2f1f0d352656f8a7982042bd85694a8e
xparta <tomarobaski@gmail.com>
2013-11-15 09:43:12 +0300

    home page and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 LostFound::Application.routes.draw do
+  root "pages#home"
+   get "about" => "pages#about"
+  # root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get "welcome/about"
-  root 'welcome#index'
+  # get "welcome/about"
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
